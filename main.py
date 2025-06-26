af993ba6c03a14e0d2146067a833d948522d516f
matt <matthewhendersonk@gmail.com>
2025-06-26 17:27:26 +0800

    cleaned code, ran another test, still need to remove local path

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def ImageToByteArray(image_path):
         width, height = img.size
         pixel_data = list(img.getdata())
         bit_array = np.array(pixel_data)
-        bit_array = 1- ((bit_array > 127).astype(int))
+        bit_array = 1 - ((bit_array > 127).astype(int))
         bit_array = bit_array.reshape((height, width))
         return bit_array
     except FileNotFoundError:
@@ -24,8 +24,6 @@ image_path = Path(Rf"C:\Users\4Matt\Documents\AsepriteExports\{imageName}.png")
 bit_array = ImageToByteArray(image_path)
 
 if bit_array is not None:
-    print(bit_array)
-    print(bit_array[0][3])
     np.savetxt("output.txt", bit_array, fmt="%d", delimiter="")
     valCounter = 0
     hexBox = []
@@ -42,16 +40,10 @@ if bit_array is not None:
             binaryString = binaryString[::-1]
             intVal = int(binaryString, 2)
             hexVal = f"0x{intVal:02x}"
-            print(binaryString)
-            print(intVal)
-            print(hexVal + "\n")
             hexBox.append(hexVal)
 
-            valCounter = valCounter + 1
-
-
-    print(f"Hex values: {valCounter}")
     hexBox = ["0x" + val[2:].upper() for val in hexBox]
+    
 with open(f"{imageName}_Hex.txt", "w") as f:
     for i in range(0, len(hexBox), 8):
         line_items = hexBox[i:i+8]
